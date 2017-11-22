ba545ecec03ea4a7e1f4bee9d9d484ec857a3c89
James Quigley <jmquigley@outlook.com>
2017-11-22 16:01:38 -0600

    Exposes the NotebookDetails interface

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 import {Artifact} from './lib/artifact';
-import {Binder} from './lib/binder';
+import {Binder, NotebookDetails} from './lib/binder';
 import {BinderManager} from './lib/bindermanager';
 
-export { Artifact, Binder, BinderManager };
+export {
+	Artifact,
+	Binder,
+	BinderManager,
+	NotebookDetails
+};
